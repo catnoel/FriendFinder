b2274ab2e26c30eae943cb4ce40994624fa3de29
Catherine Noel <catherinemnoel@gmail.com>
2017-09-26 15:54:58 -0400

    html & css

---
+++ app/public/survery.js
@@ -0,0 +1,20 @@
+
+var surveyQuestions = [
+{
+	question1: "When I think of breakfast it is usually",
+	answer1: [
+	{	1: "Making eggs and cheese",
+		2: "Making a veggie omelet while drinking a mimosa",
+		3: "Going to a restaurant and ordering whatever I feel in the moment"
+	}];
+	}
+},
+	{ question2: "Watching the show Chopped on a weekday night makes me",
+	answer2: [
+	{
+		1: "Go to bed quicker",
+		2: "stay entertained while I do errands",
+		3: "ELATED"
+	}];
+}
+}]
