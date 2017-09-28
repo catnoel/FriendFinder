3f97673be84d9ccd0d66450577141ffd58f07039
Catherine Noel <catherinemnoel@gmail.com>
2017-09-28 18:39:51 -0400

    update

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
+var PORT = 3000;
+var app = express();
+
+
+
+app.get("/", function(req,res){
+	res.sendFile(path.join(__dirname,"/app/public/home.html"));
+});
+
+app.get("/about", function(req,res){
+	res.sendFile(path.join(__dirname,"/app/public/about.html"));
+});
+
+app.listen(PORT, function(){
+	console.log("app listening on:", PORT);
+});
+
+
